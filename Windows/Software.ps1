3f2ed061030c81e1cbe7b3f6e3384322fbc74e8f
GerardoHD <gerardoherreradiaz@gmail.com>
2022-07-13 22:48:42 -0500

    Creado archivo Software.PS1

---
+++ Windows/Software.ps1
@@ -0,0 +1,94 @@
+#ACG Player
+winget install 9NBLGGH698C7
+
+#AV1 Video Extension
+winget install 9MVZQVXJBQ9V
+
+#-DevToys
+#-winget install 9PGCV4V3BK4W
+
+#Extensión de imagen RAW
+winget install 9NCTDW2W1BH8
+
+#Extensión de vídeo MPEG-2
+winget install 9N95Q1ZZPMH4
+
+#Facebook
+winget install 9WZDNCRFJ2WL
+
+#Instagram
+winget install 9NBLGGH5L9XT
+
+#Messenger
+winget install 9WZDNCRF0083
+
+#MusicBee
+winget install 9P4CLT2RJ1RS
+
+#MyIPTV Player
+winget install 9PJJ2NMBF0TR
+
+#myTube!
+winget install 9WZDNCRCWF3L
+
+#Ouga - Tumblr App
+winget install 9NBLGGH6GVHJ
+
+#Reddit
+winget install 9NS3RBQ5HV5F
+
+#Rufus:
+winget install 9PC3H3V7Q9CH
+
+#TikTok
+winget install 9NH2GPH4JZS4
+
+#Tumblr
+winget install 9NTDQP5CQG07
+
+#Twitter
+winget install 9WZDNCRFJ140
+
+#-QuickLook
+#-winget install 9NV4BS3L1H4S
+
+#-Wikipedia
+#-winget install 9MWVHFBTQ9ZQ
+
+winget install Microsoft.VC++2005Redist-x86
+winget install Microsoft.VC++2005Redist-x64
+winget install Microsoft.VC++2008Redist-x86
+winget install Microsoft.VC++2008Redist-x64
+winget install Microsoft.VC++2010Redist-x86
+winget install Microsoft.VC++2010Redist-x64
+winget install Microsoft.VC++2012Redist-x86
+winget install Microsoft.VC++2012Redist-x64
+winget install Microsoft.VC++2013Redist-x86
+winget install Microsoft.VC++2013Redist-x64
+winget install Microsoft.VC++2015-2019Redist-x86
+winget install Microsoft.VC++2015-2019Redist-x64
+
+winget install 7zip.7zip
+#-winget install AdrianAllard.FileConverter
+#-winget install Amazon.Kindle
+winget install Armin2208.WindowsAutoNightMode
+#-winget install Bitwarden.Bitwarden
+winget install calibre.calibre
+winget install clsid2.mpc-hc
+#-winget install ElectronicArts.EADesktop
+#-winget install EpicGames.EpicGamesLauncher
+winget install GIMP.GIMP
+winget install Google.Chrome
+winget install Google.Drive | Configurar letra E: para Google Drive
+winget install Intel.IntelDriverAndSupportAssistant
+winget install JackieLiu.NotepadsApp
+#-winget install Kingsoft.WPSOffice
+#-winget install Lenovo.SystemUpdate
+#-winget install Levitsky.FontBase
+#-winget install Oracle.JavaRuntimeEnvironment
+#-winget install Microsoft.Powertoys
+#-winget install Mozilla.Firefox
+winget install TheDocumentFoundation.LibreOffice
+#-winget install Ubisoft.Connect
+winget install Valve.Steam
+#-winget install Zoom.Zoom
