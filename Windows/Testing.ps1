75927bc9d1f187da7a0fb9280b5a34228d97ccde
Gerardo Herrera Diaz <Gerardoherreradiaz@gmail.com>
2022-07-16 09:32:14 -0500

    Create Testing.ps1

---
+++ Windows/Testing.ps1
@@ -0,0 +1,9 @@
+#Arctic Camo Special Edition
+winget install 9NG340XBBFH8
+
+#Forest Camo Special Edition 
+winget install 9P710JLWV4B2
+
+#Nightfall Camo Special Edition 
+winget install 9N4STQVZ62Q7
+
