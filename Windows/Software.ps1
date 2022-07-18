f27253e9838b2e0fe58332df393f8e78474ed743
GerardoHD <gerardoherreradiaz@gmail.com>
2022-07-18 08:42:42 -0500

    Añadido software

--- Windows/Software.ps1
+++ Windows/Software.ps1
@@ -1,60 +1,61 @@
+#!/bin/bash
+echo Activando UTC en hora de Windows:
+reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\TimeZoneInformation" /v RealTimeIsUniversal /d 1 /t REG_DWORD /f
+#
+echo Instalando Nexus Tools:
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/corbindavenport/nexus-tools/master/install.ps1'))
+#
+echo Instalando apps desde Microsoft Store
 #ACG Player
 winget install 9NBLGGH698C7
-
+#
 #AV1 Video Extension
 winget install 9MVZQVXJBQ9V
-
+#
 #-DevToys
 #-winget install 9PGCV4V3BK4W
-
+#
 #Extensión de imagen RAW
 winget install 9NCTDW2W1BH8
-
+#
 #Extensión de vídeo MPEG-2
 winget install 9N95Q1ZZPMH4
-
+#
 #Facebook
 winget install 9WZDNCRFJ2WL
-
+#
 #Instagram
 winget install 9NBLGGH5L9XT
-
-#Messenger
-winget install 9WZDNCRF0083
-
+#
 #MusicBee
 winget install 9P4CLT2RJ1RS
-
+#
 #MyIPTV Player
 winget install 9PJJ2NMBF0TR
-
+#
 #myTube!
-winget install 9WZDNCRCWF3L
-
+#winget install 9WZDNCRCWF3L
+#
 #Ouga - Tumblr App
-winget install 9NBLGGH6GVHJ
-
+#winget install 9NBLGGH6GVHJ
+#
 #Reddit
 winget install 9NS3RBQ5HV5F
-
-#Rufus:
+#
+#Rufus
 winget install 9PC3H3V7Q9CH
-
+#
 #TikTok
 winget install 9NH2GPH4JZS4
-
+#
 #Tumblr
 winget install 9NTDQP5CQG07
-
+#
 #Twitter
 winget install 9WZDNCRFJ140
-
-#-QuickLook
-#-winget install 9NV4BS3L1H4S
-
-#-Wikipedia
-#-winget install 9MWVHFBTQ9ZQ
-
+#
+echo Instalando DirectX y Microsoft VC
+winget install Microsoft.DirectX
 winget install Microsoft.VC++2005Redist-x86
 winget install Microsoft.VC++2005Redist-x64
 winget install Microsoft.VC++2008Redist-x86
@@ -67,9 +68,10 @@ winget install Microsoft.VC++2013Redist-x86
 winget install Microsoft.VC++2013Redist-x64
 winget install Microsoft.VC++2015-2019Redist-x86
 winget install Microsoft.VC++2015-2019Redist-x64
-
+#
+echo Instalando software desde winget
 winget install 7zip.7zip
-#-winget install AdrianAllard.FileConverter
+#-winget install AdrienAllard.FileConverter
 #-winget install Amazon.Kindle
 winget install Armin2208.WindowsAutoNightMode
 #-winget install Bitwarden.Bitwarden
@@ -79,12 +81,14 @@ winget install clsid2.mpc-hc
 #-winget install EpicGames.EpicGamesLauncher
 winget install GIMP.GIMP
 winget install Google.Chrome
-winget install Google.Drive | Configurar letra E: para Google Drive
+winget install Google.Drive
+echo Asignar G:\ para Google Drive
+winget install HulubuluSoftware.AdvancedRenamer
 winget install Intel.IntelDriverAndSupportAssistant
 winget install JackieLiu.NotepadsApp
 #-winget install Kingsoft.WPSOffice
 #-winget install Lenovo.SystemUpdate
-#-winget install Levitsky.FontBase
+winget install Levitsky.FontBase
 #-winget install Oracle.JavaRuntimeEnvironment
 #-winget install Microsoft.Powertoys
 #-winget install Mozilla.Firefox
@@ -92,3 +96,4 @@ winget install TheDocumentFoundation.LibreOffice
 #-winget install Ubisoft.Connect
 winget install Valve.Steam
 #-winget install Zoom.Zoom
+#
