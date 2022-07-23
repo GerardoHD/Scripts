8532da67cb932d6dbef810464d682219eae2218f
Gerardo Herrera Diaz <Gerardoherreradiaz@gmail.com>
2022-07-23 12:11:28 -0500

    Update Testing.ps1

--- Windows/Testing.ps1
+++ Windows/Testing.ps1
@@ -1,16 +1,20 @@
-#!/bin/bash
-#
 echo Activando UTC en hora de Windows:
 reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\TimeZoneInformation" /v RealTimeIsUniversal /d 1 /t REG_DWORD /f
 #
 echo Instalando Nexus Tools:
 iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/corbindavenport/nexus-tools/master/install.ps1'))
 #
+#Extensión de imagen RAW
+winget install 9NCTDW2W1BH8
+#
 #Extensiones de video HEVC
 start ms-windows-store://pdp/?productid= install 9NMZLZ57R3T7
 #
 #Extensiones de video HEVC del fabricante del del dispositivo
 start ms-windows-store://pdp/?productid=9N4WGH0Z6VHQ
+#
+#Rufus
+winget install 9PC3H3V7Q9CH
 #
 #Tentacles: Enter the mind
 start ms-windows-store://pdp/?productid=9WZDNCRFJB4N
@@ -20,3 +24,4 @@ start ms-windows-store://pdp/?productid=9WZDNCRDSGKG
 #
 #Where's My Water? 2
 start ms-windows-store://pdp/?productid=9WZDNCRFJCV6
+#
