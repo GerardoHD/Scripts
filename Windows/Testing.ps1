231ed13b2fc87fc100b4d25a43f6a824659f34ad
Gerardo Herrera Diaz <Gerardoherreradiaz@gmail.com>
2022-07-23 12:17:50 -0500

    Update Testing.ps1

--- Windows/Testing.ps1
+++ Windows/Testing.ps1
@@ -1,8 +1,8 @@
-echo Activando UTC en hora de Windows:
+#echo UTC en hora de Windows:
 reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\TimeZoneInformation" /v RealTimeIsUniversal /d 1 /t REG_DWORD /f
 #
-echo Instalando Nexus Tools:
-iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/corbindavenport/nexus-tools/master/install.ps1'))
+#echo Instalando Nexus Tools:
+#iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/corbindavenport/nexus-tools/master/install.ps1'))
 #
 #Extensión de imagen RAW
 winget install 9NCTDW2W1BH8
