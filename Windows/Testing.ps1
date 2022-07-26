eb71b89aff0e8978b741e9128b667f5e45bec876
Gerardo Herrera Diaz <Gerardoherreradiaz@gmail.com>
2022-07-26 08:36:14 -0500

    Añadidos nuevos temas

--- Windows/Testing.ps1
+++ Windows/Testing.ps1
@@ -1,27 +1,44 @@
-#echo UTC en hora de Windows:
-#reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\TimeZoneInformation" /v RealTimeIsUniversal /d 1 /t REG_DWORD /f
-#
-#echo Instalando Nexus Tools:
-#iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/corbindavenport/nexus-tools/master/install.ps1'))
-#
-#Extensión de imagen RAW
-#winget install 9NCTDW2W1BH8
-#
-#Extensiones de video HEVC
-#start ms-windows-store://pdp/?productid=9NMZLZ57R3T7
-#
-#Extensiones de video HEVC del fabricante del del dispositivo
-#start ms-windows-store://pdp/?productid=9N4WGH0Z6VHQ
-#
-#Rufus
-#winget install 9PC3H3V7Q9CH
-#
-#Tentacles: Enter the mind
-start ms-windows-store://pdp/?productid=9WZDNCRFJB4N
-#
-#Torrex Pro
-#start ms-windows-store://pdp/?productid=9WZDNCRDSGKG
-#
-#Where's My Water? 2
-#start ms-windows-store://pdp/?productid=9WZDNCRFJCV6
-#
+#Reclaimed by Nature
+winget install 9N38HN1P66Z5
+
+#Architectural Structures PREMIUM
+winget install 9PNR72H74000
+
+#Abstract Bubbles PREMIUM
+winget install 9NBJJWNJ4CFC
+
+#Abstract Art Premium
+winget install 9MW76BGZQ211
+
+#Calligraphy
+winget install 9ND1WTDSQ1PP
+
+#Fibonacci Sequence in Nature
+winget install 9PCK43KQ0GHW
+
+#Near Side of the Moon
+winget install 9PG292SCN0CS
+
+#Ocean Plastic Mouse
+winget install 9N5D70GSX8GK
+
+#Opposite Day
+winget install 9N94D2W0S4HK
+
+#Panoramic Train Views PREMIUM
+winget install 9NWQZT7QGFH6
+
+#Trains on Bridges PREMIUM
+winget install 9PHVJ1FMB72Q
+
+#Land Down Under by Chad Copeland
+winget install 9N9TGS29LV89
+
+#Remix 3D Animals
+winget install 9PBDB440SWLC
+
+#Bending Light PREMIUM
+winget install 9NVFRQ51NHPC
+
+#Ice Crystals PREMIUM
+winget install 9NXNHBV1DD41
