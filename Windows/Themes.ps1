5da6697dd954e0c0231b91bf7eb2d68cc9b429c2
GerardoHD <gerardoherreradiaz@gmail.com>
2022-07-25 22:48:32 -0500

    Creado Script para instlar temas

---
+++ Windows/Themes.ps1
@@ -0,0 +1,42 @@
+#Aerial Views PREMIUM
+winget install 9PH9843DT9FH
+#Arctic Camo Special Edition
+winget install 9NG340XBBFH8
+#Bing Fan Favorites II 
+winget install 9NL02W3ZR06D
+#Colors of Holi PREMIUM
+winget install 9P4VCCZQQ1K2
+#Forest Camo Special Edition 
+winget install 9P710JLWV4B2
+#Ninja Cat Holiday Escape
+winget install 9MSQ49ZGVTKG
+#Nightfall Camo Special Edition 
+winget install 9N4STQVZ62Q7
+#Paint 3D Creatures 
+winget install 9NB2V6XWB5G2
+#Pantone Color of the Year 2022
+winget install 9NJW9j87LCVP
+#Pride 2019 
+winget install 9N6SK33V4VGB
+#Pride 2020 Conversations
+winget install 9P1P7VLNLTN6
+#Pride 2020 Flags
+winget install 9PK6DD3SHC26
+#Remix 3D Winterscapes
+winget install 9PDGMVBBRVPH
+#Seasonal Art PREMIUM
+winget install 9N5K6FVJ26SD
+#Springtime Art
+winget install 9NB94BHPHHTV
+#The Adventures of Ninja Cat 
+winget install 9P9WG52XPKND
+#The Adventures of Ninja Cat Beyond
+winget install 9P4LLS1MX7TF
+#The Wonderful Moment 
+winget install 9N5CVTQS1MVK
+#Vector Art PREMIUM
+winget install 9NS8ZZWGP9KN
+#What Next PREMIUM 
+winget install 9MXB9F5PW5BJ
+#Windows Throwback
+winget install 9NJ1LP5X2MJM
