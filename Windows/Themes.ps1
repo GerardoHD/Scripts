126ae109d868ca0041329b04c49cb8c5044d2469
Gerardo Herrera Diaz <Gerardoherreradiaz@gmail.com>
2022-07-26 09:09:01 -0500

    Añadidos más temas

--- Windows/Themes.ps1
+++ Windows/Themes.ps1
@@ -1,42 +1,105 @@
+#Abstract Art Premium
+winget install 9MW76BGZQ211
+#
+#Abstract Bubbles PREMIUM
+winget install 9NBJJWNJ4CFC
+#
 #Aerial Views PREMIUM
 winget install 9PH9843DT9FH
+#
+#Architectural Structures PREMIUM
+winget install 9PNR72H74000
+#
 #Arctic Camo Special Edition
 winget install 9NG340XBBFH8
+#
 #Bing Fan Favorites II 
 winget install 9NL02W3ZR06D
-#Colors of Holi PREMIUM
-winget install 9P4VCCZQQ1K2
+#
+#Bending Light PREMIUM
+winget install 9NVFRQ51NHPC
+#
+#Calligraphy
+winget install 9ND1WTDSQ1PP
+#
+#Fibonacci Sequence in Nature
+winget install 9PCK43KQ0GHW
+#
 #Forest Camo Special Edition 
 winget install 9P710JLWV4B2
+#
+#Ice Crystals PREMIUM
+winget install 9NXNHBV1DD41
+#
+#Land Down Under by Chad Copeland
+winget install 9N9TGS29LV89
+#
+#Near Side of the Moon
+winget install 9PG292SCN0CS
+#
 #Ninja Cat Holiday Escape
 winget install 9MSQ49ZGVTKG
+#
 #Nightfall Camo Special Edition 
 winget install 9N4STQVZ62Q7
+#
+#Ocean Plastic Mouse
+winget install 9N5D70GSX8GK
+#
+#Opposite Day
+winget install 9N94D2W0S4HK
+#
 #Paint 3D Creatures 
 winget install 9NB2V6XWB5G2
+#
+#Panoramic Train Views PREMIUM
+winget install 9NWQZT7QGFH6
+#
 #Pantone Color of the Year 2022
 winget install 9NJW9j87LCVP
+#
 #Pride 2019 
 winget install 9N6SK33V4VGB
+#
 #Pride 2020 Conversations
 winget install 9P1P7VLNLTN6
+#
 #Pride 2020 Flags
 winget install 9PK6DD3SHC26
+#
+#Reclaimed by Nature
+winget install 9N38HN1P66Z5
+#
+#Remix 3D Animals
+winget install 9PBDB440SWLC
+#
+#Remix 3D Ocean Life
+winget install 9NF6WS9X6ZJT
+#
 #Remix 3D Winterscapes
 winget install 9PDGMVBBRVPH
+#
 #Seasonal Art PREMIUM
 winget install 9N5K6FVJ26SD
+#
 #Springtime Art
 winget install 9NB94BHPHHTV
+#
 #The Adventures of Ninja Cat 
 winget install 9P9WG52XPKND
+#
 #The Adventures of Ninja Cat Beyond
 winget install 9P4LLS1MX7TF
-#The Wonderful Moment 
-winget install 9N5CVTQS1MVK
+#
+#Trains on Bridges PREMIUM
+winget install 9PHVJ1FMB72Q
+#
 #Vector Art PREMIUM
 winget install 9NS8ZZWGP9KN
+#
 #What Next PREMIUM 
 winget install 9MXB9F5PW5BJ
+#
 #Windows Throwback
 winget install 9NJ1LP5X2MJM
+#
